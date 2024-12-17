9835cd387b95da3c54537d6c78ffc7b7fe2e0d82
Administrator Regula <administratorregula@xcode.regula.local>
2024-12-17 13:23:34 +0300

    Release 7.5.10926

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["DocType"]),
     ],
     targets: [
-        .binaryTarget(name: "DocType", url: "https://pods.regulaforensics.com/DocType/7.4.9821/DocumentReaderCore_doctype_7.4.9821.zip", checksum: "4503ec57ee3885c9421ccb48ee8757fec342fe05e89f3f3c293de38df72e1e58"),
+        .binaryTarget(name: "DocType", url: "https://pods.regulaforensics.com/DocType/7.5.10926/DocumentReaderCore_doctype_7.5.10926.zip", checksum: "d3ce0faa24b880a360e5e016470f2d2157063eec4afe920dbd49e19fe6e4f3bf"),
     ]
 )
