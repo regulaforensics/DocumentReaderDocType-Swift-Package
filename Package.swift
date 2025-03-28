6e120e04847a9cc92cab7289646dbf766280d35f
jenkins <jenkins@regula.by>
2025-03-29 00:26:48 +0300

    Release 7.6.11787

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["DocType"]),
     ],
     targets: [
-        .binaryTarget(name: "DocType", url: "https://pods.regulaforensics.com/DocType/7.5.11014/DocumentReaderCore_doctype_7.5.11014.zip", checksum: "9b5795e784d9ff67dd5ccb1dfb1fda7d09d9f4c3439610e062ba1346aec8a3f2"),
+        .binaryTarget(name: "DocType", url: "https://pods.regulaforensics.com/DocType/7.6.11787/DocumentReaderCore_doctype_7.6.11787.zip", checksum: "5566ae268d7e6ed0d0e0862a1c0516042a27a20f327053247004f2d0a4a0b3e0"),
     ]
 )
