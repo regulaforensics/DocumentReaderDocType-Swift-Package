e67dfc6be85b8d6c13a38fbc96b292008380fd46
Jenkins <jenkins@regula.by>
2026-09-02 03:50:07 +0300

    RC 9.8.20231

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["DocTypeStage"]),
     ],
     targets: [
-        .binaryTarget(name: "DocTypeStage", url: "https://pods.regulaforensics.com/Stage/DocTypeStage/9.8.20211/DocumentReaderCoreStage_doctype_9.8.20211.zip", checksum: "e73686c1123c9dad5e22f1ee0f498165d7cbbd17e8f88a309fd1ae3a44dd4df6"),
+        .binaryTarget(name: "DocTypeStage", url: "https://pods.regulaforensics.com/Stage/DocTypeStage/9.8.20231/DocumentReaderCoreStage_doctype_9.8.20231.zip", checksum: "0e3edc864cddfd32faea28b3f8fe2181d3848845b03c8f7a840a591f052d4b84"),
     ]
 )
